eec5b230d8a07eea066585eb9e689973c027ae0d
R HARSHAVARDHAN <harsha29052001@gmail.com>
2020-06-27 11:10:02 +0530

    sdkv

--- orist/settings.py
+++ orist/settings.py
@@ -116,9 +116,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-prod_db = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
@@ -126,3 +123,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
